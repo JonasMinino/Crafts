65866910bdf8d1693fdaf3e2bef12ebbf7aa6f3f
jonasminino <jonasminino@gmail.com>
2021-01-28 12:01:43 -0500

    Added the Jason File Product Service

---
+++ Crafts/Services/JasonFileProductService.cs
@@ -0,0 +1,34 @@
+﻿using Crafts.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Crafts.Services
+{
+    public class JasonFileProductService
+    {
+        public JasonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFile { get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); } }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFile))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
